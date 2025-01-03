9680fa5dfd87ea27ea98c38f919d2537ca804b17
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2025-01-03 20:57:24 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyComponent from './components/myComponent';
+
+function App() {
+  return (
+    <div >
+      Hello word
+      {Math.random()}
+      <MyComponent></MyComponent>
+    </div>
+
+  );
+}
+
+export default App;
